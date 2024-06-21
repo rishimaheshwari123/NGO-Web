9d8caa720ebe91ccf33db468564f944c6c34a36b
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-06-21 11:39:46 +0530

    .

---
+++ client/src/components/LangaugeSwither.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+function LanguageSwitcher() {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (e) => {
+    const selectedLanguage = e.target.value;
+    i18n.changeLanguage(selectedLanguage);
+  };
+
+  return (
+    <div className="language-selection text-black">
+      <select onChange={changeLanguage} value={i18n.language}>
+        <option value="en" className=' text-black'>English</option>
+        <option value="hi" className=' text-black'>Hindi</option>
+      </select>
+    </div>
+  );
+}
+
+export default LanguageSwitcher;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,13 +4,14 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { useTranslation } from "react-i18next";
+
 const slides = [
   {
     image: "https://i.ibb.co/5k4jBfh/11.jpg",
   },
   {
-    image:
-      "https://i.ibb.co/yhcBJvy/Whats-App-Image-2024-06-17-at-12-15-45-297373d4.jpg",
+    image: "https://i.ibb.co/yhcBJvy/Whats-App-Image-2024-06-17-at-12-15-45-297373d4.jpg",
   },
   {
     image: "https://i.ibb.co/qWCRzyv/13.jpg",
@@ -27,36 +28,37 @@ const slides = [
 ];
 
 const Home = () => {
-  const [category, setCategoy] = useState([]);
+  const { t } = useTranslation();
+  const [category, setCategory] = useState([]);
 
   const getAllCategory = async () => {
     try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_BASE_URL}/category/getAll`
-      );
+      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/category/getAll`);
       if (response?.data?.success) {
-        setCategoy(response.data.categorys);
+        setCategory(response.data.categorys);
       }
     } catch (error) {
       console.log("Could not fetch category from frontend", error);
     }
   };
+
   useEffect(() => {
     getAllCategory();
   }, []);
+
   return (
     <>
       <Navbar />
+   
       <div className="pt-[100px] w-screen">
         <Slider slides={slides} />
       </div>
       <br />
-
       <div className="grid">
         <p className="text-2xl uppercase text-center my-10 font-semibold lg:text-4xl lg:font-semibold">
-          Know more about{" "}
-          <span className="text-cyan-500">Team Pahal Bareli</span>
-        </p>{" "}
+          {t('know_more')}{" "}
+          <span className="text-cyan-500">{t('team_name')}</span>
+        </p>
         <div className="grid lg:grid-cols-2 gap-5 max-w-7xl mx-auto ">
           <iframe
             className="w-full h-full sm:w-80 md:w-96 lg:w-[560px] lg:h-[315px]"
@@ -81,67 +83,40 @@ const Home = () => {
       <div className="grid gap-16 lg:gap-16 lg:grid-cols-2 max-w-7xl mx-auto px-8 mt-10 lg:my-20">
         <div className="w-[80vw] lg:h-[100vh] lg:w-full">
           <img
-            src="https://i.ibb.co/tDYtm7Q/Whats-App-Image-2024-06-10-at-10-57-34-6754bbb9.jpg"
-            alt="not found"
-            className="rounded-lg shadow-2xl shadow-red-500 object-cover lg:h-[65vh]  lg:-rotate-6"
+            src="https://i.ibb.co/tDYtm7Q/Whats-App-Image-2024-06-10-at-10-57-25.jpg"
+            className="w-full lg:w-[80%] h-auto"
+            alt=""
           />
         </div>
-        <div>
-          <p className="text-2xl font-semibold lg:text-4xl lg:font-semibold">
-            <span className="text-cyan-500">Team Pahal Bareli</span> Foundation
-            for helping poor people
-          </p>
-          <p className="text-[16px] mt-2 leading-relaxed">
-            We are driven by a singular mission: to extend a helping hand to
-            those in need, fostering a community of care and compassion. Our
-            initiatives encompass a wide range of services aimed at addressing
-            various aspects of societal welfare. From providing free tiffin
-            services to ensuring no one goes hungry, to supporting Wardha Ashram
-            as a sanctuary for many seeking refuge, our commitment knows no
-            bounds. Additionally, we organize food distribution drives, reaching
-            out to the homeless and destitute with essential supplies. Moreover,
-            our compassion extends to animals through our dedicated animal care
-            initiatives, ensuring even the voiceless receive the love and care
-            they deserve. At Team Pahal Bareli Foundation, we firmly believe in
-            the power of collective effort and community support. Together, we
-            strive to create a world where everyone has access to basic
-            necessities and the opportunity to lead dignified lives. Join us in
-            our mission to make a meaningful difference in the lives of those
-            less fortunate.
-          </p>
+        <div className="text-gray-700 p-4 text-justify lg:text-xl leading-7 tracking-wide font-light">
+          <h2 className="text-2xl font-semibold lg:text-4xl text-center">
+            {t('foundation_helping')}
+          </h2>
+          <p className="pt-4 text-xl font-light">{t('mission_statement')}</p>
         </div>
       </div>
-
-      {/* <Causes /> */}
-      <div className="max-w-7xl mx-auto px-5 my-20">
-        <p className="text-cyan-500 uppercase mb-12 text-2xl font-semibold lg:text-4xl lg:font-semibold text-center">
-          our culture
-        </p>
-        <div className="grid gap-5 lg:grid-cols-3 md:grid-cols-2">
-          {category.length > 0 ? (
-            category.map((currElem, index) => (
-              <Link to={`/product?id=${currElem._id}`} key={index}>
-                <div
-                  className="card p-4 border shadow-xl shadow-yellow-500 flex flex-col justify-between h-full"
-                  key={currElem.id}
-                >
-                  <img
-                    src={currElem.image}
-                    alt="not found"
-                    className="rounded-lg hover:opacity-75 h-48 w-full object-cover"
-                  />
-                  <p className="text-center text-2xl lg:text-3xl font-semibold mt-4">
-                    {currElem.title}
-                  </p>
+      <div className="my-10 max-w-7xl mx-auto">
+        <h2 className="text-center text-2xl lg:text-4xl font-semibold">
+          {t('our_culture')}
+        </h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
+          {category.length ? (
+            category.map((cat) => (
+              <Link
+                key={cat._id}
+                to={`/category/${cat.slug}`}
+                className="block text-center text-lg"
+              >
+                <div className="p-4 bg-gray-200 rounded-lg shadow">
+                  <h3 className="text-xl font-semibold">{cat.name}</h3>
                 </div>
               </Link>
             ))
           ) : (
-            <p className="flex m-auto">No category found</p>
+            <p className="col-span-4 text-center text-xl">{t('no_category_found')}</p>
           )}
         </div>
       </div>
-
       <Footer />
     </>
   );
